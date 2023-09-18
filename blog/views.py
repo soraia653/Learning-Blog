fbe006f4b828ded5e276595a104d7f7ab780bd8e
Soraia Tavares <76966637+soraia653@users.noreply.github.com>
2023-09-18 11:36:27 +0000

    added missing component

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.db import IntegrityError
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.contrib import messages
+from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
