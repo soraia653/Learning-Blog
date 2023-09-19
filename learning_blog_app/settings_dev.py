72d3228adb8ba3bdd0937b5754895f2cfcbb680d
Soraia Tavares <76966637+soraia653@users.noreply.github.com>
2023-09-19 12:57:19 +0000

    created workflow to upload settings when commiting to prod

--- learning_blog_app/settings_dev.py
+++ learning_blog_app/settings_dev.py
@@ -14,7 +14,7 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(".env.dev")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,10 +145,3 @@ CKEDITOR_CONFIGS = {
         'allowedContent': True,
         },
     }
-
-# CSRF_COOKIE_SECURE = True
-# SESSION_COOKIE_SECURE = True
-# SECURE_HSTS_SECONDS = 60
-# SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-# SECURE_SSL_REDIRECT = True
-# SECURE_HSTS_PRELOAD = True
